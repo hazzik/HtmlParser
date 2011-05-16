e39bb6e32fdc042df95844d841825faccc30c78c
hazzik <hazzik@gmail.com>
2011-05-16 23:52:16 +0600

    refactoring

--- Class1.cs
+++ Class1.cs
@@ -13,7 +13,7 @@ namespace ClassLibrary3
         public void ParseNodeHtml()
         {
             const string html = "<html></html>";
-            HtmlNode node = Parse(html);
+            HtmlNode node = Parse(html).First();
             Assert.Equal("html", node.Name);
         }
 
@@ -21,38 +21,54 @@ namespace ClassLibrary3
         public void ParseNodeBody()
         {
             const string html = "<body></body>";
-            HtmlNode node = Parse(html);
+            HtmlNode node = Parse(html).First();
             Assert.Equal("body", node.Name);
         }
 
-        private static HtmlNode Parse(string html)
+        [Fact]
+        public void Parse2Tags()
         {
-            var tokens = ParseTokens(html);
+            const string html = "<head></head><body></body>";
+            IEnumerable<HtmlNode> nodes = Parse(html);
+            HtmlNode first = nodes.First();
+            HtmlNode last = nodes.Last();
+            Assert.Equal("head", first.Name);
+            Assert.Equal("body", last.Name);
+        }
+        [Fact]
+        public void ParseTextAndTag()
+        {
+            const string html = "<<head></head>";
+            IEnumerable<HtmlNode> nodes = Parse(html);
+            HtmlNode first = nodes.First();
+            HtmlNode last = nodes.Last();
+            Assert.Equal("<", first.Name);
+            Assert.Equal("head", last.Name);
+        }
+
+        private static IEnumerable<HtmlNode> Parse(string html)
+        {
+            IEnumerable<Token> tokens = ParseTokens(html);
             return BuildTree(tokens);
         }
 
         private static IEnumerable<Token> ParseTokens(string html)
         {
-            var previousToken = new Token(TokenType.Text);
-            char previousChar = '\0';
-            ParseState state = ParseState.Text;
-            var currentToken = new Token(TokenType.Text);
-            var tokens = new List<Token> {currentToken};
+            var context = new Context();
+            var tokens = new List<Token>();
             foreach (char ch in html)
             {
                 if (ch == '<')
                 {
-                    switch (state)
+                    switch (context.State)
                     {
                         case ParseState.Default:
                         case ParseState.Text:
-                            previousToken = currentToken;
-                            currentToken = new Token(TokenType.OpenTag);
-                            tokens.Add(currentToken);
-                            state = ParseState.Tag;
+                            tokens.Add(context.CurrentToken);
+                            context.SwitchState(ParseState.Tag, TokenType.OpenTag);
                             break;
                         case ParseState.Tag:
-                            previousToken.Builder.Append(previousChar);
+                            context.PreviousToken.Builder.Append(context.PreviousChar);
                             break;
                         default:
                             throw new ArgumentOutOfRangeException();
@@ -60,17 +76,16 @@ namespace ClassLibrary3
                 }
                 else if (ch == '>')
                 {
-                    switch (state)
+                    switch (context.State)
                     {
                         case ParseState.Default:
                         case ParseState.Text:
-                            currentToken.Builder.Append(ch);
+                            context.CurrentToken.Builder.Append(ch);
                             break;
                         case ParseState.Tag:
-                            previousToken = currentToken;
-                            currentToken = new Token(TokenType.Text);
-                            tokens.Add(currentToken);
-                            state = ParseState.Default;
+                            if (context.CurrentToken.IsNotEmpty())
+                                tokens.Add(context.CurrentToken);
+                            context.SwitchState(ParseState.Default, TokenType.Text);
                             break;
                         default:
                             throw new ArgumentOutOfRangeException();
@@ -78,14 +93,14 @@ namespace ClassLibrary3
                 }
                 else if (ch == '/')
                 {
-                    switch (state)
+                    switch (context.State)
                     {
                         case ParseState.Default:
                         case ParseState.Text:
-                            currentToken.Builder.Append(ch);
+                            context.CurrentToken.Builder.Append(ch);
                             break;
                         case ParseState.Tag:
-                            currentToken.Type = TokenType.CloseTag;
+                            context.CurrentToken.Type = TokenType.CloseTag;
                             break;
                         default:
                             throw new ArgumentOutOfRangeException();
@@ -93,18 +108,20 @@ namespace ClassLibrary3
                 }
                 else
                 {
-                    currentToken.Builder.Append(ch);
+                    context.CurrentToken.Builder.Append(ch);
                 }
-                previousChar = ch;
+                context.PreviousChar = ch;
             }
-            return tokens;
+
+            tokens.Add(context.CurrentToken);
+            return tokens.Where(x => x.IsNotEmpty());
         }
 
-        private static HtmlNode BuildTree(IEnumerable<Token> tokens)
+        private static IEnumerable<HtmlNode> BuildTree(IEnumerable<Token> tokens)
         {
             var tree = new List<HtmlNode>();
             var currentNode = new HtmlNode();
-            foreach (var token in tokens)
+            foreach (Token token in tokens)
             {
                 switch (token.Type)
                 {
@@ -120,13 +137,43 @@ namespace ClassLibrary3
                     case TokenType.AttributeValue:
                         break;
                     case TokenType.Text:
+                        tree.Add(new HtmlNode {Name = token.Builder.ToString()});
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
             }
-            return tree.First();
+            return tree;
         }
+
+        #region Nested type: Context
+
+        private class Context
+        {
+            public Context()
+            {
+                State = ParseState.Default;
+                PreviousToken = new Token(TokenType.Text);
+                CurrentToken = new Token(TokenType.Text);
+            }
+
+            public ParseState State { get; private set; }
+
+            public Token PreviousToken { get; private set; }
+
+            public char PreviousChar { get; set; }
+
+            public Token CurrentToken { get; private set; }
+
+            public void SwitchState(ParseState state, TokenType tokenType)
+            {
+                State = state;
+                PreviousToken = CurrentToken;
+                CurrentToken = new Token(tokenType);
+            }
+        }
+
+        #endregion
 
         #region Nested type: HtmlNode
 
@@ -165,6 +212,11 @@ namespace ClassLibrary3
             public StringBuilder Builder
             {
                 get { return builder; }
+            }
+
+            public bool IsNotEmpty()
+            {
+                return Builder.Length > 0;
             }
         }
 
